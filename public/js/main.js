d1b66db85176cd725dec23eadce1ef1bc31d7715
Rohit Sagar <rohitsagar.256@gmail.com>
2019-04-06 14:29:42 +0530

    fixed

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,19 @@
 // ==================================================================================
 $(document).ready(function(){
 
-    
-
+    // marqueeJs
+    $('.marquee').marquee({
+        //speed in milliseconds of the marquee
+        duration: 12000,
+        //gap in pixels between the tickers
+        gap: 50,
+        //time in milliseconds before the marquee will start animating
+        delayBeforeStart: 0,
+        //'left' or 'right'
+        direction: 'left',
+        //true or false - should the marquee be duplicated to show an effect of continues flow
+        duplicated: true,
+        allowCss3Support: true,
+        pauseOnHover: true
+    });
 });
